2fe7cff217cf9c3fbf54035c167d67fdd7c6335e
sabrina97490 <sabrina.mouedine@gmail.com>
2022-10-12 14:04:26 +0400

    ajout favicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     './resources/**/*.blade.php',
     './resources/**/*.js',
     './resources/**/*.vue',
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
   ],
   theme: {
     extend: {},
